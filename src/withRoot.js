1e67f10b9ca9fd4619d4c8a006e6b2590039030b
Rasheed <a.rasheedalabi@gmail.com>
2020-07-23 14:51:48 +0100

    setup react app with i18n and cypress

---
+++ src/withRoot.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { create } from "jss";
+import rtl from "jss-rtl";
+import i18n from "./i18n/i18n";
+
+import {
+  MuiThemeProvider,
+  StylesProvider,
+  createMuiTheme,
+  jssPreset,
+} from "@material-ui/core/styles";
+
+const jss = create({ plugins: [...jssPreset().plugins, rtl()] });
+
+function withRoot(Component) {
+  function WithRoot(props) {
+    return (
+      <StylesProvider jss={jss}>
+        <MuiThemeProvider
+          theme={createMuiTheme({
+            direction: i18n.dir(),
+          })}
+        >
+          <Component {...props} />
+        </MuiThemeProvider>
+      </StylesProvider>
+    );
+  }
+
+  return WithRoot;
+}
+
+export default withRoot;
